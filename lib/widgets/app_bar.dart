df82c61a11fcd7405e53dbcc1e1d47ed9c02d4f3
Youssef Darahem <youssef.darahem@gmail.com>
2022-09-04 08:19:08 +0300

    app bar added

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const MyAppBar({Key? key}) : super(key: key);
+  final Widget logo;
+
+  const MyAppBar({Key? key, required this.logo}) : super(key: key);
 
   @override
   Size get preferredSize => const Size.fromHeight(80);
@@ -12,66 +16,28 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
         padding: EdgeInsets.only(
           top: MediaQuery.of(context).padding.top,
         ),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          gradient: const LinearGradient(
             begin: Alignment.centerLeft,
             end: Alignment.centerRight,
-            colors: [
-              Color.fromARGB(255, 3, 142, 139),
-              Color.fromARGB(255, 67, 117, 153)
-            ],
+            colors: mainColorsGradients,
           ),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Center(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    const Icon(
-                      Icons.location_on,
-                      color: Colors.white,
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Text(
-                          'YateemAC',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18),
-                        ),
-                        Text(
-                          'Donate And Track',
-                          style: TextStyle(color: Colors.white, fontSize: 10),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: const <Widget>[
-                  Icon(Icons.notifications_none_outlined, color: Colors.white),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Icon(Icons.menu, color: Colors.white),
-                ],
-              ),
-            ],
-          ),
-        )
-        // color: Colors.blue,
-        );
+        child: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: logo,
+            ),
+            const Spacer(),
+            const Icon(Icons.notifications_none_outlined, color: Colors.white),
+            const SizedBox(width: 10),
+            const Icon(Icons.menu, color: Colors.white),
+            const SizedBox(width: 10),
+          ],
+        ));
   }
 }
