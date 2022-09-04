bfc2a4068c3897aeb406e36860c3351d149f02ef
Youssef Darahem <youssef.darahem@gmail.com>
2022-09-04 08:11:29 +0300

    constants created

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const mainColorsGradients = [
+  Color.fromARGB(255, 3, 142, 133),
+  Color.fromARGB(255, 6, 140, 139),
+  Color.fromARGB(255, 17, 136, 142),
+  Color.fromARGB(255, 35, 129, 146),
+  Color.fromARGB(255, 61, 119, 151),
+  Color.fromARGB(255, 67, 117, 153),
+];
